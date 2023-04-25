6bf8b00ce57b416fcf0654e17b4340e5e2cb9cf8
Javen Wilson <javenb022@gmail.com>
2023-04-25 12:18:26 -0400

    Refactor: add second biker to test requirements for log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,7 @@ require "./lib/biker"
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
     @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
@@ -28,21 +29,38 @@ RSpec.describe Biker do
 
   describe "#log_ride(ride, time)" do
     it "adds a ride and time to the bikers ride log" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
       @biker.log_ride(@ride2, 60.9)
       @biker.log_ride(@ride2, 61.6)
+      @biker2.log_ride(@ride1, 97.0)
+      @biker2.log_ride(@ride2, 67.0)
 
-      expected = {
+      expected1 = {
         @ride1 => [92.5, 91.1],
         @ride2 => [60.9, 61.6]
       }
-      expect(@biker.rides).to eq(expected)
+      expect(@biker.rides).to eq(expected1)
+      expect(@biker2.rides).to eq({})
+
+      @biker2.learn_terrain!(:gravel)
+      @biker2.learn_terrain!(:hills)
+      @biker2.log_ride(@ride1, 95.0)
+      @biker2.log_ride(@ride2, 65.0)
+
+      expected2 = {
+        @ride2 => [65.0]
+      }
+      expect(@biker2.rides).to eq(expected2)
     end
   end
 
   describe "#personal_record(ride)" do
     it "calls on the bikers fastest time on a ride" do
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
       @biker.log_ride(@ride1, 92.5)
       @biker.log_ride(@ride1, 91.1)
       @biker.log_ride(@ride2, 60.9)
