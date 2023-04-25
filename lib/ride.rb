a9ec8755df92472b72e6d567bd9042f96d69b675
Javen Wilson <javenb022@gmail.com>
2023-04-25 11:12:53 -0400

    Add attributes, loop, and total distance methods

--- lib/ride.rb
+++ lib/ride.rb
@@ -1,3 +1,24 @@
 class Ride
+  attr_reader :name,
+              :distance,
+              :terrain,
+              :loop
+  def initialize(info)
+    @name = info[:name]
+    @distance = info[:distance]
+    @terrain = info[:terrain]
+    @loop = info[:loop]
+  end
 
+  def loop?
+    @loop
+  end
+
+  def total_distance
+    if @loop == true
+      @distance
+    else
+      @distance * 2
+    end
+  end
 end
