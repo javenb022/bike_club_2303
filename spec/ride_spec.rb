587211b91db406c28b621c605ed6995921b7ab76
Javen Wilson <javenb022@gmail.com>
2023-04-25 11:17:15 -0400

    Refactor: harden loop and total_distance with another ride

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -3,22 +3,29 @@ require "./lib/ride"
 RSpec.describe Ride do
   before(:each) do
     @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
-
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
-  describe '#initialize' do
-    it 'exists with attributes' do
+  describe "#initialize" do
+    it "exists with attributes" do
       expect(@ride1).to be_a(Ride)
       expect(@ride1.name).to eq("Walnut Creek Trail")
       expect(@ride1.distance).to eq(10.7)
       expect(@ride1.terrain).to eq(:hills)
+    end
+  end
+
+  describe "#loop?" do
+    it "determines if ride is a loop" do
       expect(@ride1.loop?).to eq(false)
+      expect(@ride2.loop?).to eq(true)
     end
   end
 
   describe "#total_distance" do
     it "can calculate total distance of a ride" do
       expect(@ride1.total_distance).to eq(21.4)
+      expect(@ride2.total_distance).to eq(14.9)
     end
   end
 end
