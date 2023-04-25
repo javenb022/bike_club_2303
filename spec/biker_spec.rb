f5aee247d37583952e2ef58be611a79da3833ecf
Javen Wilson <javenb022@gmail.com>
2023-04-25 11:53:00 -0400

    Add tests for personal_record

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -40,4 +40,16 @@ RSpec.describe Biker do
       expect(@biker.rides).to eq(expected)
     end
   end
+
+  describe "#personal_record(ride)" do
+    it "calls on the bikers fastest time on a ride" do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expect(@biker.personal_record(@ride1)).to eq(91.1)
+      expect(@biker.personal_record(@ride2)).to eq(60.9)
+    end
+  end
 end
