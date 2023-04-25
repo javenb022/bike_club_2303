784a27064490aab33a961b15c17c3ea2a1a7eba2
Javen Wilson <javenb022@gmail.com>
2023-04-25 11:46:31 -0400

    Add tests for log_ride

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,8 @@ require "./lib/biker"
 RSpec.describe Biker do
   before(:each) do
     @biker = Biker.new("Kenny", 30)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
   end
 
   describe "#initialize" do
@@ -21,6 +23,21 @@ RSpec.describe Biker do
       @biker.learn_terrain!(:hills)
 
       expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+  end
+
+  describe "#log_ride(ride, time)" do
+    it "adds a ride and time to the bikers ride log" do
+      @biker.log_ride(@ride1, 92.5)
+      @biker.log_ride(@ride1, 91.1)
+      @biker.log_ride(@ride2, 60.9)
+      @biker.log_ride(@ride2, 61.6)
+
+      expected = {
+        @ride1 => [92.5, 91.1],
+        @ride2 => [60.9, 61.6]
+      }
+      expect(@biker.rides).to eq(expected)
     end
   end
 end
