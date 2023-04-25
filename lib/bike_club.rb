1344480c4309fa81c7868754ee84497db719756f
Javen Wilson <javenb022@gmail.com>
2023-04-25 12:58:43 -0400

    Add most_rides method

--- lib/bike_club.rb
+++ lib/bike_club.rb
@@ -9,4 +9,16 @@ class BikeClub
   def add_biker(biker)
     @bikers << biker
   end
+
+  def most_rides
+    num_of_rides = Hash.new(0)
+    @bikers.each do |biker|
+      biker.rides.sum do |ride|
+        num_of_rides[biker] += ride[1].count
+      end
+    end
+    num_of_rides.max_by do |biker, rides|
+      return biker if rides = num_of_rides.values.max
+    end
+  end
 end
