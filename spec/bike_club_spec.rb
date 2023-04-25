4e590ae35de55082203c7137d8a65f04b2a47248
Javen Wilson <javenb022@gmail.com>
2023-04-25 12:33:21 -0400

    Add tests for add_biker method

--- spec/bike_club_spec.rb
+++ spec/bike_club_spec.rb
@@ -5,6 +5,8 @@ require "./lib/bike_club"
 RSpec.describe BikeClub do
   before(:each) do
     @bike_club1 = BikeClub.new("Hell's Angels")
+    @biker1 = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
   end
 
   describe "#initialize" do
@@ -12,6 +14,14 @@ RSpec.describe BikeClub do
       expect(@bike_club1).to be_a(BikeClub)
       expect(@bike_club1.name).to eq("Hell's Angels")
       expect(@bike_club1.bikers).to eq([])
+    end
+  end
+
+  describe "#add_biker(biker)" do
+    it "adds a biker to the bike clubs bikers" do
+      expect(@bike_club1.bikers).to eq([])
+
+      expect(@bike_club1.add_biker(@biker1)).to eq([@biker1])
     end
   end
 end
